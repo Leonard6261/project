2aa9225213e668fcaf1e8444d72e3567e96bc367
Leonard6261 <88305372+Leonard6261@users.noreply.github.com>
2022-01-28 00:06:17 +0800

    Add files via upload

---
+++ resume/config/config.js
@@ -0,0 +1,122 @@
+var config = {
+    
+    name: "Yuxuan Zheng",
+    sex: "Male",
+    age: "20",
+    phone: "+86-15851056261",
+    email: "121040083@link.cuhk.edu.cn",
+    address: "No.2001, Longxiang Avenue, Longgang District, Shenzhen,China",
+    qq: "yz17u20soton",
+    log: "Leonard",
+    excpect_work: "Frond End/Data Analysis",
+
+
+    
+    motto: [
+        "When life hands you lemons, make lemonade.",
+        "Act as both an inspiration and a calming force",
+        "Be the change that you wish to see in the world.",
+        "We are what we repeatedly do.",
+        "Do or do not. There is no try.",
+        "While I breathe, I hope.",
+        "Live every day to the fullest – in moderation.",
+        "Be who you were meant to be.",
+        "You have to be odd to be number one.",
+        "You’ll miss the best things if you keep your eyes shut.",
+        "No one has ever become poor by giving.",
+        "Don’t be busy: be productive.",
+        "The grass is green where you water it.",
+        "Stand straight, walk proud, have a little faith."
+    ],
+
+
+    
+    welcome: "青青子衿，悠悠我心<br>" +
+             "但为君故，沉吟至今<br>" +
+             "Hello! My name is Yuxuan Zheng, a student currently studying at The Chinese University of Hong Kong, Shenzhen.<br>" +
+             "Nice to meet you!",
+
+
+  
+    about: "<p>Hello! My name is Yuxuan Zheng, a student currently studying at The Chinese University of Hong Kong, Shenzhen. My future career expectation is Front End/Data Analysis</p>" +
+        "<p>I have related experience in Python/Javascript coding. Moreover, I have a passionate work attitude and can work with groups efficiently to find and solve problems. </p>" +
+        "<p>I am really forward to getting your contact!</p>",
+
+
+
+    
+    skills: [
+        ["Tableau", 75, "red"],
+        ["Python", 80, "blue"],
+        ["SQL", 75, "#1abc9c"],
+        ["HTML5", 67, "rgba(0,0,0)"],
+        ["CSS3", 60, "yellow"],
+        ["JavaScript", 65, "pink"]
+    ],
+
+
+    
+    skills_description: "<ul>" +
+        "     <li>Basic knowledge on computer science,</li>" +
+        "     <li>Understand the use of Git when cooperating with a group to develop.</li>" +
+        "     <li>Familiarize with the algorithms for basic data structures and related programming methodology.</li>" +
+        "     <li>Familiarize with the use of HTML5/CSS3/Javascript.</li>" +
+        "<li>Familiarize with the use of SQL statements and its optimization in practice.</li>" +
+        "     <li>Understand the use of data visualization tools, e.g. Tableau, to analyze large-size datasets.</li>" +
+        
+        "     <li>Unerstand the use of Matlab/Geogebra/Latex.</li>" +
+        "     <li>Good collaboration & communication capability with other divisions.</li>" +
+        "     <li>Be responsible and proactive</li>" +
+        "   </ul>",
+
+
+ 
+     
+    portfolio: [
+        ["./images/fistula.jpg", "https://leonard6261.github.io/project/1/analysis.html", "Project Fistula", "A website that I research by using Tableau to visualize the Fistula disease."],
+        ["./images/renter9.png", "https://rentersz.com", "Renter", "The second-hand commodities trading platform I maintained with other three CUHKSZ students on phone."],
+        ["./images/sicp.png", "https://github.com/Leonard6261/sicp", "Meta-Circular Evaluator", "The End Assignment of the SICP Program at NUS"]
+    ],
+
+
+   
+    work: [
+     
+
+        ["October 2021-present", "<br>Front-end Developer Intern (Parttime, Paid, 2hrs/week)",
+            "<p><strong>Shenzhen Renter Leasing Co., Ltd. (Student Startup at CUHK-Shenzhen)</strong></p>" +
+            "<p>Co-develop the front-end of a mini program embedded in WeChat for Renter, support routine maintenance and iteration, maintain SQL database.</p>" +
+            "<p>Use Collarborating with other members to maintain and update the program distantly by using Git. </p>"
+        ],
+
+        ["2020/7/1 — 2021/8/10", "<br>Student Assistant of Official Website Operation (Parttime, Paid, 2hrs/week)",
+            "<p><strong>Student Affairs Office at CUHK-Shenzhen</strong></p>" +
+            "<p>Assist the maintenance of the official webpages of the Office, update contents, report bugs, and propose solutions.</p>" +
+            "<p>Focus on the Human-Computer-Interaction(HCI) optimization of the website.</p>"  
+        ]
+    ],
+
+
+    
+    others: [
+        ["July 2021", "SICP Excellent Rune Artist (top 3 out of 40)", "This is an award that were given in the SICP program competition where my designed Rune picture by using Javascript was voted the third most."],
+        ["November 2019", "Second Prize (top 10 out of 200) in the English Speech Competition at Xi’an Jiaotong University Suzhou Academy", "This was awarded by my successful performance during the English speech competition when I attended the International Preparatory Program in Suzhou"],
+        ["November 2018", "Outstanding Volunteer in 2018 Nanjing Marathon", "Awarded for the on-time and high-quality support for the marathon candidates."],
+        ["July 2018", "Third Prize in the National Secondary School Student Innovative Writing Contest", "This is a Chinese Writing Award given in a writing competiton hosted by The writing Academy of China."]
+    ],
+
+
+   
+    
+
+
+    
+    url: [
+        //背景图、头像、作品展示背景、其他经历背景
+        "./images/intro-bg.jpg",
+        "./images/potrait.jpg",
+        "./images/work-bk.png",
+        "./images/4.jpg"
+    ]
+
+}
